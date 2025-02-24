54b37dba08f97c67e91ef364acf14edd3ba6d4b7
jack@kilmer.co <=>
2025-02-23 23:31:11 -0600

    move models to index and get

--- src/actions/models.cr
+++ src/actions/models.cr
@@ -3,9 +3,27 @@ require "http/client"
 require "../models/response/model"
 
 class Kai::Actions::Models
-  def send
+  def index
     response = HTTP::Client.get(
       "https://api.x.ai/v1/models",
     )
+
+    if response.success?
+      return Kai::Models::Response::Model.from_json(response.body)
+    else
+      raise "Failed to get models: #{response.body}"
+    end
+  end
+
+  def show(id : String)
+    response = HTTP::Client.get(
+      "https://api.x.ai/v1/models/#{id}",
+    )
+
+    if response.success?
+      return Kai::Models::Response::Model.from_json(response.body)
+    else
+      raise "Failed to get model: #{response.body}"
+    end
   end
 end
